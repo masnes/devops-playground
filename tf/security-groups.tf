a76ee5aafd43e5c24fbbe7d5d2c0e8d21cba9324
Michael Asnes <mike.asnes@gmail.com>
2020-04-04 17:06:14 -0600

    break things out a bit more

---
+++ tf/security-groups.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_security_group" "base" {
+  name        = "base"
+  description = "all access needs from my apex apartment"
+
+  dynamic "ingress" {
+    for_each = [22, 8000, 8089]
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
+      cidr_blocks = [
+        "73.78.216.54/32",
+        "73.229.170.110/32"
+      ]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
