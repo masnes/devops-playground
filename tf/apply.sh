f80c5c7c83ed94801f997b21457d1c5469c0edc2
Michael Asnes <mike.asnes@gmail.com>
2020-04-04 00:10:23 -0600

    always apply even if terraform fail
    attempt 2

--- tf/apply.sh
+++ tf/apply.sh
@@ -29,9 +29,9 @@ fi
 git add *.tf
 git commit -m "pre terraform $cmd" || true
 git pull --rebase
-( set +e
+set +e
 terraform "$cmd" -auto-approve
-)
+set -e
 git add terraform.tfstate *.tf
 git commit -m "terraform $cmd run" || true
 git pull --rebase
