12ff9e696b295cedebd7f9407c3e53742fdba5da
Michael Asnes <mike.asnes@gmail.com>
2020-04-04 00:01:45 -0600

    automate git steps around terraform

---
+++ tf/apply.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+usage() {
+  echo $0 "[a - pply | d - estroy]"
+}
+
+
+case $1 in
+  a*)
+    cmd="apply"
+  ;;
+  d*)
+    cmd="destroy"
+  ;;
+  *)
+    usage
+    exit 1
+  ;;
+esac
+
+set -x
+( cd "$script_dir/"  # make sure we are in the tf directory for this part
+if [ -f ./terraform.tfstate ]; then
+  git add terraform.tfstate
+fi
+git add *.tf
+git commit -m "pre terraform $cmd" || true
+git pull --rebase
+terraform "$cmd" -auto-approve
+git add terraform.tfstate *.tf
+git commit -m "terraform $cmd run" || true
+git pull --rebase
+git push
+)
