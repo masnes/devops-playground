d0146e2fd26bb6ead7a02f467a53c95f32e818af
Michael Asnes <mike.asnes@gmail.com>
2020-04-21 23:10:51 -0600

    add ansible runner command

--- ansible/run-ansible.sh
+++ ansible/run-ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 script_dir="$(dirname "$(readlink -f "$0")")"
-export TF_STATE="$script_dir/tf/"
-ansible-playbook --inventory-file="$script_dir"/terraform-inventory  ca-server.yml
+export TF_STATE="$script_dir/../tf/"
+ansible-playbook --inventory-file="$script_dir"/terraform-inventory  "$script_dir"/ca-server.yml
