d4ec2353315727edf345fcf3ffd0a3f490d2f746
Michael Asnes <mike.asnes@gmail.com>
2020-04-21 22:17:34 -0600

    add usage message

--- temp-setup.sh
+++ temp-setup.sh
@@ -14,5 +14,29 @@ at "now + $ADDITIONAL_TIME" <<EOF
 "$script_dir/tf/apply.sh" destroy
 EOF
 )
+usage() {
+  echo "Setup, then teardown in 30 minutes:"
+  echo
+  echo "$0"
+  echo
+  echo "You may provide a time interval. See \`man 1 at\` for all options."
+  echo
+  echo "Some examples:"
+  echo
+  echo "$0" "'30 minutes'"
+  echo "$0" "'2 hours'"
+  echo "$0" "'1 days'"
+  echo
+  echo "If run more than once, shutdown attempts will trigger at all given time intervals"
+}
+
+case "$1" in
+  [Hh]*|?*)
+    usage
+    exit
+    ;;
+  *)
+    ;;
+esac
 
 "$script_dir/tf/apply.sh" apply
