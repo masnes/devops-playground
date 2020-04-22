58bc33a3a32ee2a3530071738f55e17488fd594b
Michael Asnes <mike.asnes@gmail.com>
2020-04-21 22:34:34 -0600

    give the ca server a dns address

--- tf/ca-server.tf
+++ tf/ca-server.tf
@@ -1,3 +1,8 @@
+variable "user_route53_hosted_zone" {
+  type    = string
+  default = "Z1TOKUY3VKZYQI" # *.michaelasnes.com.
+}
+
 resource "aws_instance" "ca_server" {
   ami             = "ami-00f4b9641119301bf" # Debian 10 x86
   instance_type   = "t3a.nano"
@@ -20,6 +25,18 @@ resource "aws_instance" "ca_server" {
       "echo '${aws_key_pair.old_laptop_default.public_key}' >> ~/.ssh/authorized_keys",
     ]
   }
+}
+
+resource "aws_eip" "ca_ip" {
+  instance = aws_instance.ca_server.id
+}
+
+resource "aws_route53_record" "ca_server" {
+  zone_id = var.user_route53_hosted_zone
+  name    = "ca-server.michaelasnes.com"
+  type    = "A"
+  ttl     = 300
+  records = []
 }
 
 output "ca_server" {
