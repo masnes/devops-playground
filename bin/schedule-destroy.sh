08e31add4493ccdff857156b53e05a5c56d1b5c6
Michael Asnes <mike.asnes@gmail.com>
2020-06-07 23:16:46 -0600

    auto reset teardown schedule

--- bin/schedule-destroy.sh
+++ bin/schedule-destroy.sh
@@ -2,6 +2,18 @@
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
+job_contains_destroy_canary() {
+  job="$1"
+  at -c "$job" | grep -q 'DESTROY_CANARY'
+}
+
+while read -r "job" ; do
+  if job_contains_destroy_canary "$job" ; then
+    atrm "$job"
+    echo "Cancelled previously scheduled teardown."
+  fi
+done <<<"$(atq | awk '{print $1}')"
+
 ADDITIONAL_TIME="30 minutes"
 if [[ -n "$1" ]]; then
   ADDITIONAL_TIME="$1"
@@ -11,6 +23,7 @@ echo "Will teardown setup in $ADDITIONAL_TIME"
 set -x
 at "now + $ADDITIONAL_TIME" <<EOF
 "$script_dir/../tf/apply.sh" destroy
+# DESTROY_CANARY
 EOF
 )
 
--- temp-setup.sh
+++ temp-setup.sh
@@ -30,4 +30,7 @@ esac
 
 "$script_dir"/bin/schedule-destroy.sh "$1"
 "$script_dir/tf/apply.sh" apply
+SLEEP=10
+echo "AWS DNS is quick, but give it $SLEEP seconds to propagate"
+sleep $SLEEP
 "$script_dir/ansible/run-ansible.sh"
