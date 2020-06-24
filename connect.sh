71cc6d64ec83f92158bba56461c0e1e3e6d259be
Michael Asnes <mike.asnes@gmail.com>
2020-06-23 23:15:45 -0600

    use terraform inventory to connect

--- connect.sh
+++ connect.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 script_dir="$(dirname "$(readlink -f "$0")")"
-cd "$script_dir/tf"
+
+usage() {
+  cat <<EOF
+Usage:
+  $0 username servername_in_tf_config
+
+Example:
+  $0 admin ca_server
+EOF
+}
+if [[ $# -ne 2 ]]; then
+  usage
+  exit 1
+fi
+target_server="$(TF_STATE="$script_dir/tf/" ./ansible/terraform-inventory --list | jq ".$2[0]" | tr -d '"')"
 set -x
-ssh "$1"@"$(terraform output "$2")"
+ssh "$1"@"$target_server"
