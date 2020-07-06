460d8e1d397ded92301568efb1338d062e762639
Michael Asnes <mike.asnes@gmail.com>
2020-07-06 01:35:00 -0600

    pre terraform apply

--- tf/networking.tf
+++ tf/networking.tf
@@ -3,16 +3,16 @@ resource "aws_vpc" "devops_playground" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.devops_playground.id
-  cidr_block = "172.16.0.0/17" # ends at 172.16.127.255
+  vpc_id            = aws_vpc.devops_playground.id
+  cidr_block        = "172.16.0.0/17" # ends at 172.16.127.255
   availability_zone = "us-east-2"
 
-  map_public_ip_on_launch = false  # It's the default, but make explicit here
+  map_public_ip_on_launch = false # It's the default, but make explicit here
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.devops_playground.id
-  cidr_block = "172.16.128.0/24"  # ends at 172.16.128.255
+  vpc_id            = aws_vpc.devops_playground.id
+  cidr_block        = "172.16.128.0/24" # ends at 172.16.128.255
   availability_zone = "us-east-2"
 }
 
@@ -21,7 +21,7 @@ resource "aws_eip" "public_nat_ip" {
 }
 
 resource "aws_nat_gateway" "public_nat" {
-  subnet_id = aws_subnet.public.id
+  subnet_id     = aws_subnet.public.id
   allocation_id = aws_eip.public_nat_ip.id
 }
 
@@ -31,7 +31,7 @@ resource "aws_route_table" "public_private_table" {
   # default route for vpc cidr to local is implicitly created
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.public_nat.id
   }
 }
