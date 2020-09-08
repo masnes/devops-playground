314540120f3cf1a2d04fd220b6a6c5a044d3a983
Michael Asnes <mike.asnes@gmail.com>
2020-09-07 19:50:40 -0600

    more distinct destroy canary

--- bin/schedule-destroy.sh
+++ bin/schedule-destroy.sh
@@ -5,7 +5,7 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 
 job_contains_destroy_canary() {
   job="$1"
-  at -c "$job" | grep -q 'DESTROY_CANARY'
+  at -c "$job" | grep -q 'DESTROY_CANARY_DEVOPS_PLAYGROUND'
 }
 
 while read -r "job" ; do
@@ -24,7 +24,7 @@ echo "Will teardown setup in $ADDITIONAL_TIME"
 set -x
 at "now + $ADDITIONAL_TIME" <<EOF
 "$script_dir/../tf/apply.sh" destroy
-# DESTROY_CANARY
+# DESTROY_CANARY_DEVOPS_PLAYGROUND
 EOF
 )
 
