d260ed2990e5fd53132851e06dc2b1fa97bfa287
Michael Asnes <mike.asnes@gmail.com>
2020-09-27 13:46:34 -0600

    inventory query helper

---
+++ query-inventory.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+usage() {
+  cat <<EOF
+Usage:
+  $0
+  $0 servername_in_tf_config
+
+Examples:
+  $0 # get all data
+  $0 ca_server  # get data for one instance
+EOF
+}
+if [[ $# -eq 0 ]]; then
+  TF_STATE="$script_dir/tf/" ./ansible/terraform-inventory --list | jq "."
+elif [[ $# -eq 1 ]]; then
+  TF_STATE="$script_dir/tf/" ./ansible/terraform-inventory --list | jq ".$1[0]" | tr -d '"'
+else
+  usage
+  exit 1
+fi
