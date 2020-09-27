61edaca9c151b46500cd81e10d8c23678702ab29
Michael Asnes <mike.asnes@gmail.com>
2020-09-26 19:22:34 -0600

    pre terraform apply

--- tf/security-groups.tf
+++ tf/security-groups.tf
@@ -34,9 +34,10 @@ resource "aws_security_group" "public_servers" {
     ]
   }
 
+
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
